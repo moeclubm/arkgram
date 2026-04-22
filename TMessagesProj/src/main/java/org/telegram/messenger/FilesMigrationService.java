ccd074b878888c942a20978f6a3c0b6eb49ceebd
MoeclubM <qytc233@gmail.com>
2026-04-23 03:22:23 +0800

    fix: restore outline airplane branding icons

--- TMessagesProj/src/main/java/org/telegram/messenger/FilesMigrationService.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/FilesMigrationService.java
@@ -61,7 +61,7 @@ public class FilesMigrationService extends Service {
         Notification notification = new Notification.Builder(this, NotificationsController.OTHER_NOTIFICATIONS_CHANNEL)
                 .setContentTitle(getText(R.string.MigratingFiles))
                 .setAutoCancel(false)
-            .setSmallIcon(R.drawable.notification_flex)
+                .setSmallIcon(R.drawable.notification)
                 .build();
 
         isRunning = true;
@@ -175,7 +175,7 @@ public class FilesMigrationService extends Service {
                 Notification notification = new Notification.Builder(FilesMigrationService.this, NotificationsController.OTHER_NOTIFICATIONS_CHANNEL)
                         .setContentTitle(getText(R.string.MigratingFiles))
                         .setContentText(String.format("%s/%s", currentCount, totalFilesCount))
-                    .setSmallIcon(R.drawable.notification_flex)
+                        .setSmallIcon(R.drawable.notification)
                         .setAutoCancel(false)
                         .setProgress(totalFilesCount, currentCount, false)
                         .build();
